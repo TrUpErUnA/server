ce35648d3adecc32f4ef44c360ad3fde5deef016
Timur <orlovitma88@gmail.com>
2025-04-16 16:56:41 +0300

    Изменил порт на server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 10000;  // используем переменную окружения для порта
+const port = process.env.PORT || 2000;  // используем переменную окружения для порта
 
 // Настройка для обработки JSON
 app.use(express.json());
@@ -11,10 +11,16 @@ app.post('/sensor', (req, res) => {
    const sensorData = req.body;
    console.log('Данные от датчика:', sensorData);
 
-   // Генерация сообщения для Алисы
-   const message = `Концентрация частиц PM1.0 (SDS_P1): ${sensorData.sensordatavalues.find(v => v.value_type === 'SDS_P1').value}, Концентрация частиц PM2.5 (SDS_P2): ${sensorData.sensordatavalues.find(v => v.value_type === 'SDS_P2').value}`;
+   // Проверка наличия данных перед использованием find
+   const sdsP1 = sensorData.sensordatavalues ? sensorData.sensordatavalues.find(v => v.value_type === 'SDS_P1') : null;
+   const sdsP2 = sensorData.sensordatavalues ? sensorData.sensordatavalues.find(v => v.value_type === 'SDS_P2') : null;
 
-   res.json({ message });
+   if (sdsP1 && sdsP2) {
+      const message = `Концентрация частиц PM1.0 (SDS_P1): ${sdsP1.value}, Концентрация частиц PM2.5 (SDS_P2): ${sdsP2.value}`;
+      res.json({ message });
+   } else {
+      res.status(400).json({ error: 'Не удалось получить данные о концентрации частиц' });
+   }
 });
 
 // Запуск сервера
